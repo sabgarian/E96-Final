26d0aac66f2eb05af53ef994c02c3fb48105d193
sabgarian <stefan.abgarian@gmail.com>
2024-05-21 11:46:06 -0700

    Implemented first part of typer script and moved MuteButton.cs to scripts folder

--- Assets/Scripts/Typer.cs
+++ Assets/Scripts/Typer.cs
@@ -1,18 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Typer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // WordBank here
+    public Text wordOutput = null;
+
+    private string remainingWord = string.Empty;
+    private string currentWord = "test word";
+
+    private void Start()
     {
-        
+        SetCurrentWord();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetCurrentWord()
     {
-        
+        // Get word from bank
+        SetRemainingWord(currentWord);
+    }
+
+    private void SetRemainingWord(string newString)
+    {
+        remainingWord = newString;
+        wordOutput.text = remainingWord;
+    }
+
+    private void Update()
+    {
+        CheckInput();
+    }
+
+    private void CheckInput()
+    {
+        if(Input.anyKeyDown)
+        {
+            string keysPressed = Input.inputString;
+
+            if (keysPressed.Length == 1)
+            {
+                EnterLetter(keysPressed);
+            }
+        }
+    }
+
+    private void EnterLetter(string typedLetter)
+    {
+        if(IsCorrectLetter(typedLetter))
+        {
+            RemoveLetter();
+
+            if(IsWordComplete())
+            {
+                SetCurrentWord();
+            }
+        }
+    }
+
+    private bool IsCorrectLetter(string letter)
+    {
+        return remainingWord.IndexOf(letter) == 0;
+    }
+
+    private void RemoveLetter()
+    {
+        string newString = remainingWord.Remove(0, 1);
+        SetRemainingWord(newString);
+    }
+    
+    private bool IsWordComplete()
+    {
+        return remainingWord.Length == 0;
     }
 }
