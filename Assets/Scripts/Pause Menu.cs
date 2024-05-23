978401444a78c765a9c07de2e1d740243198a3df
tali-bd <taliblando@gmail.com>
2024-05-23 10:39:13 -0700

    added code to pause menu

---
+++ Assets/Scripts/Pause Menu.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PausePanel;
+    public GameObject PauseButton;
+    public GameObject ResumeButton;
+    public static bool isPaused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PausePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused) 
+            {
+                ResumeGame(); 
+            }
+
+            else
+            { 
+                PauseGame(); 
+            }   
+        }
+    }
+
+    public void PauseGame()
+    {
+        PausePanel.SetActive(true);
+        Time.timeScale = 0;
+        isPaused = true;
+    }   
+    
+    public void ResumeGame()
+    {
+        PausePanel.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+}
