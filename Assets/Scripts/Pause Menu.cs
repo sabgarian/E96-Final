344fddbeaa33462cc860f26f08ab37c70d73c849
tali-bd <taliblando@gmail.com>
2024-05-23 10:49:27 -0700

    trying to get pause menu to work (still doesn't)
    doesn't read my pausepanel object as the variable

--- Assets/Scripts/Pause Menu.cs
+++ Assets/Scripts/Pause Menu.cs
@@ -17,8 +17,7 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
-        {
+        
             if(isPaused) 
             {
                 ResumeGame(); 
@@ -28,7 +27,7 @@ public class PauseMenu : MonoBehaviour
             { 
                 PauseGame(); 
             }   
-        }
+       
     }
 
     public void PauseGame()
