65c6df70efa193be849864941af2a2e79811cb72
sabgarian <stefan.abgarian@gmail.com>
2024-05-30 13:07:03 -0700

    transitions to memento1 upon completing first word bank

--- Assets/Scripts/Typer.cs
+++ Assets/Scripts/Typer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
 public class Typer : MonoBehaviour
 {
